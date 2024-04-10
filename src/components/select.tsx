04a06b8c25e667c06e28bd460e96941fdd6bd998
Gustavo Sales <gussales14@gmail.com>
2024-04-10 17:33:15 -0300

    fix: corrigido bug ao selecionar o quarto 1 em Reservar um quarto

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,18 +1,18 @@
 import { useBookings } from "@/hooks/useBookings"
-import { ComponentProps, useEffect } from "react"
+import { ComponentProps } from "react"
 
 type SelectRoomsProps = ComponentProps<'select'>
 
 export function SelectRooms(props: SelectRoomsProps) {
   const { avaliableRooms } = useBookings()
-
-  useEffect(() => {}, [avaliableRooms])
+  console.log(avaliableRooms);
 
   return(
     <select 
       {...props}
       className="flex h-10 w-fit items-center justify-between rounded-md border border-input bg-background cursor-pointer px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1"
     >
+      <option value="" disabled selected className="disabled:hidden">Selecione o quarto</option>
       {
         avaliableRooms.map(item => (
           <option 
